5a4171174bbb28fc0ee4d6e6d62f45fd1de555a1
Mohsin Firoz <44004341+MohsinFiroz@users.noreply.github.com>
2025-02-11 10:47:48 +0000

    fix

--- deployment/scripts/setup-nginx.sh
+++ deployment/scripts/setup-nginx.sh
@@ -2,8 +2,8 @@
 sudo apt-get update
 sudo apt-get install -y nginx certbot python3-certbot-nginx
 
-# # Copy Nginx configuration files
-# sudo cp /deployment/docker/nginx/conf.d/*.conf /etc/nginx/conf.d/
+# Copy Nginx configuration files
+sudo cp temp/deployment/docker/nginx/conf.d/*.conf /etc/nginx/conf.d/
 
 # Obtain SSL certificates
 sudo certbot --nginx \
