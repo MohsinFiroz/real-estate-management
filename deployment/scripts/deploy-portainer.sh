982c671ca21b3d8a593101a5cdfeffbb72b9f7eb
Mohsin Firoz <m7firoz@gmail.com>
2025-03-11 01:24:33 +1100

    trying minio deployment

--- deployment/scripts/deploy-portainer.sh
+++ deployment/scripts/deploy-portainer.sh
@@ -9,7 +9,7 @@ docker volume create portainer_data
 docker run -d \
   --name portainer \
   --restart always \
-  -p 9000:9000 \
+  -p 1000:1000 \
   -v /var/run/docker.sock:/var/run/docker.sock \
   -v portainer_data:/data \
   portainer/portainer-ce:latest
