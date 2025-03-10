2b443222fc4bc26eee090580fb9fadc6b4ea2f16
Mohsin Firoz <m7firoz@gmail.com>
2025-03-11 04:02:40 +1100

    trying minio deployment

--- deployment/scripts/deploy-minio.sh
+++ deployment/scripts/deploy-minio.sh
@@ -3,7 +3,6 @@
 # Parameters
 MINIO_USER=$1
 MINIO_PASSWORD=$2
-DOMAIN=$3
 
 # Pull the latest MinIO image
 docker pull minio/minio:latest
@@ -27,9 +26,6 @@ docker run -d \
   -v ~/minio/config:/root/.minio \
   -e "MINIO_ROOT_USER=${MINIO_USER}" \
   -e "MINIO_ROOT_PASSWORD=${MINIO_PASSWORD}" \
-  -e "MINIO_BROWSER_REDIRECT_URL=https://minio-console.softcelia.com" \
-  -e "MINIO_SERVER_URL=https://minio.softcelia.com" \
   minio/minio:latest server /data --console-address ":9101"
 
 echo "MinIO deployed successfully on ports 9100 (API) and 9101 (Console)"
-echo "Access the MinIO Console at http://$DOMAIN:9101"
